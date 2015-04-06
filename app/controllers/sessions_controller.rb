fe3c74d81daeb0be0bac8c6ce29e2b8783bbbbf8
Ashari Juang <asharijuang@me.com>
2015-04-06 15:01:17 +0700

    fix style and fix session controller

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -6,9 +6,9 @@ class SessionsController < ApplicationController
 	 	user = User.authenticate_user(params[:email], params[:password])
 	 	if user
 	      do_login(user)
-	      redirect_to user_url(user), notice: 'Login Successfully'
+	      redirect_to root_url, notice: 'Login Successfully'
 		else
-	      redirect_to login_url, alert: 'Invalid Email / Password'
+	      render 'new' , alert: 'Invalid Email / Password'
 		end
 	end
 	def destroy
